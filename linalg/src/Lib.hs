2ec741c808e9837fa4798710cf2c90e7f716a80d
Aaron Toderash (adt150) <aaron.t@usask.ca>
2019-02-18 15:37:56 -0600

    Used `stack new linalg` to initialize the project skeleton

--- linalg/src/Lib.hs
+++ linalg/src/Lib.hs
@@ -1,15 +1,6 @@
 module Lib
-  ( outputMatrix
-  ) where
+    ( someFunc
+    ) where
 
-data Matrix = [Fractional] | [[Fractional]]
--- TODO: Instance definitions for show??
-
-outputMatrix :: Matrix->IO()
-outputMatrix m   = print m
-
---TODO: Will the following definition work now?
---formatMatrix :: Matrix->String
---formatMatrix [] = []
---formatMatrix (x:xs) = x:(formatMatrix xs)
-
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
