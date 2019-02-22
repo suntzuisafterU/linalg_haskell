b8aa6a6fe69792828bb9cf5c5ef2cefac1f0a23d
Aaron Toderash (adt150) <aaron.t@usask.ca>
2019-02-21 22:31:59 -0600

    TODOs in FracSpec.hs

--- linalg/test/FracSpec.hs
+++ linalg/test/FracSpec.hs
@@ -7,6 +7,16 @@ import Test.QuickCheck
 import Test.QuickCheck.All
 import Frac
 
--- spec :: Spec
--- spec = do
---   describe "Addition" $ do 
+-- TODO: Add tests that use <statement> `shouldReturn` Left "something here"
+-- TODO: <statement> `shouldSatisfy` (not . null) -- <- any predicate
+-- TODO: <statement> `shouldThrow` anyException -- <- or other exception names (ex, anyErrorCall, anyIOException, anyArithException)
+-- TODO: define own selector predicate.  (ex, <statement> `shouldThrow` (== ExitFailure 1))
+
+
+spec :: Spec
+spec = do
+  describe "Addition" $ do
+    -- TODO: Figure out how to write good QuickCheck properties
+    -- it "All the addition" $ property $
+    --   \x y -> (x + y) 
+
