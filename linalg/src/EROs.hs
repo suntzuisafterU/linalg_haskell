9887ee45c2534389d8941d9ee53b66d9849afe53
adt150 <adt150@mail.usask.ca>
2019-02-25 18:59:30 -0600

    EROs module

---
+++ linalg/src/EROs.hs
@@ -0,0 +1,13 @@
+module ElementaryRowOps where
+
+import Mat
+
+-- swapRows :: Integral a => Mat -> a -> a -> Mat
+
+-- Flow: Original Matrix -> Row being manipulated -> Row to use for reduction -> Modified Matrix
+-- addMultiplOfRow :: Integral a => Mat -> a -> a -> (Factor: Rational??Could be float as well) -> Mat
+
+-- multiplyRowByFactor :: Integral a => Mat -> a -> (Factor: Rational??Could be float as well) -> Mat
+
+-- swapColumns :: Integral a => Mat -> a -> a -> Mat ?? TODO: Is this a valid elementary op?
+
